a95473a33d4193ff1f05cca5a205d92e72d0de3a
Arux-BTT <arnocoin@gmx.fr>
2015-12-28 11:39:58 +0100

    Added handling of Ctrl+C and other signals on Windows, and removed a TODO comment about something already done.

--- main.c
+++ main.c
@@ -130,7 +130,6 @@ void *PoolBroadcastThreadProc(void *Info)
 	
 	for(;;)
 	{
-		// TODO/FIXME: Use nanosleep().
 		while(pthread_mutex_trylock(&QueueMutex)) Sleep(1);
 		for(Share *CurShare = RemoveShare(&CurrentQueue); CurShare; CurShare = RemoveShare(&CurrentQueue))
 		{
@@ -1124,6 +1123,8 @@ void *MinerThreadProc(void *Info)
 	
 	return(NULL);
 }
+
+#ifdef __linux__
 	
 void SigHandler(int signal)
 {
@@ -1133,6 +1134,20 @@ void SigHandler(int signal)
 	
 	pthread_mutex_unlock(&Mutex);
 }
+#else
+
+BOOL SigHandler(DWORD signal)
+{
+	pthread_mutex_lock(&Mutex);
+	
+	ExitFlag = true;
+	
+	pthread_mutex_unlock(&Mutex);
+	
+	return(TRUE);
+}
+
+#endif
 
 // Signed types indicate there is no default value
 // If they are negative, do not set them.
@@ -1396,6 +1411,7 @@ void FreeSettings(AlgoSettings *Settings)
 // that the connection may be cleanly closed.
 
 // TODO: Get Platform index from somewhere else
+// TODO/FIXME: Check functions called for error.
 int main(int argc, char **argv)
 {
 	PoolInfo Pool;
@@ -1425,7 +1441,11 @@ int main(int argc, char **argv)
 	ExitHandler.sa_handler = SigHandler;
 	
 	sigaction(SIGINT, &ExitHandler, NULL);
+
+	#else
 	
+	SetConsoleCtrlHandler((PHANDLER_ROUTINE)SigHandler, TRUE);
+		
 	#endif
 	
 	RestartMining = (atomic_bool *)malloc(sizeof(atomic_bool) * Settings.TotalThreads);
